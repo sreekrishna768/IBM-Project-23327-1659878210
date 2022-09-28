5d6e359fa4e7cde13a547bc4aed99b03957da042
vaishnavi chockalingam <68457139+vaishnavi-2103@users.noreply.github.com>
2022-09-28 17:50:33 +0530

    Update update_delete_usertable.sql

--- ASSESSMENTS/Team Member 1-Vaishnavi.C/Assignment-3/update_delete_usertable.sql
+++ ASSESSMENTS/Team Member 1-Vaishnavi.C/Assignment-3/update_delete_usertable.sql
@@ -1,9 +1,9 @@
 --2.perform UPDATE,DELETE queries with the user table
 
 --inserting items in  user table
-INSERT INTO `accounts` (`id`, `username`, `password`,  `rollno`  ,   `email`) VALUES (1, 'vaishnavi', 'vaishu12345', 'cse001','vaishnavi.chocklaingam@gmail.com');
-INSERT INTO `accounts` (`id`, `username`, `password`, `rollno`, `email`) VALUES (2, 'hemalatha', 'hema12','cse002', 'hemalatha.chocku@gmail.com');
-select * from accounts;
+INSERT INTO `user` (`id`, `username`, `password`,  `rollno`  ,   `email`) VALUES (1, 'vaishnavi', 'vaishu12345', 'cse001','vaishnavi.chocklaingam@gmail.com');
+INSERT INTO `user` (`id`, `username`, `password`, `rollno`, `email`) VALUES (2, 'hemalatha', 'hema12','cse002', 'hemalatha.chocku@gmail.com');
+select * from user;
 
 
 --update the user table
@@ -11,8 +11,8 @@ update user  set rollno='19cse001' where id=1;
 
 
 --deleting the user table
-delete from accounts where  email='vaishnavi.chockalingam@gmail.com';
+delete from user where  email='vaishnavi.chockalingam@gmail.com';
 
 --viewing the table
 
-select * from accounts;
+select * from user;
