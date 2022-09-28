5417c4abe56a7d390429738ee9753d69f5abb682
vaishnavi chockalingam <68457139+vaishnavi-2103@users.noreply.github.com>
2022-09-28 16:53:45 +0530

    Create create_user_table.sql

---
+++ ASSESSMENTS/Team Member 1-Vaishnavi.C/Assignment-3/create_user_table.sql
@@ -0,0 +1,16 @@
+
+1.//create user table with user with email,username,roll number,password
+
+CREATE DATABASE vaishu
+USE `vaishu`;
+CREATE TABLE user(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  	`username` varchar(50) NOT NULL,
+  	`password` varchar(255) NOT NULL,
+    	`rollnumber` varchar(255) NOT NULL,
+    
+  	`email` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) 
+select * from accounts;
+
